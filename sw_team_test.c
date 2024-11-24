f5de2bb60fcc596ccbcf511571a333c417bba584
minjin0610 <138571857+minjin0610@users.noreply.github.com>
2024-11-25 00:34:10 +0900

    Update sw_team_test.c

--- sw_team_test.c
+++ sw_team_test.c
@@ -4,7 +4,7 @@
 #include "sw_team.h"
 
 void setUp(void) {
-    srand(time(NULL)); // �׽�Ʈ ȯ�� �ʱ�ȭ
+    srand(time(NULL)); // 테스트 환경 초기화
 }
 
 typedef struct {
@@ -23,7 +23,7 @@ void setup_test_case(TestCase tc) {
         }
     }
 
-    // �ܰ��� ��ֹ� ��ġ
+    // 외곽에 벽 설정
     for (int i = 0; i < 20; i++) {
         map[0][i][0] = 1;
         map[19][i][0] = 1;
@@ -31,25 +31,24 @@ void setup_test_case(TestCase tc) {
         map[i][19][0] = 1;
     }
 
-    // �κ� �ʱ� ��ġ
+    // 로봇 초기 위치 설정
     robot_location[0] = 10;
     robot_location[1] = 10;
-    robot_rotation = 0; // Facing up
-    map[10][10][0] = 2; // Place robot
+    robot_rotation = 0; // 위쪽을 향함
+    map[10][10][0] = 2; // 로봇 위치 표시
 
-    // TestCase ���� ���� �ֺ� ��ֹ�/���� ����
-    if (tc.front) map[9][10][0] = 1; // ��ֹ�: ����
-    if (tc.left)  map[10][9][0] = 1; // ��ֹ�: ����
-    if (tc.right) map[10][11][0] = 1; // ��ֹ�: ����
-    if (tc.dust)  map[10][10][1] = 1; // ����: ���� ��ġ
+    // TestCase에 따라 주변 장애물 및 먼지 설정
+    if (tc.front) map[9][10][0] = 1; // 장애물: 앞쪽
+    if (tc.left)  map[10][9][0] = 1; // 장애물: 왼쪽
+    if (tc.right) map[10][11][0] = 1; // 장애물: 오른쪽
+    if (tc.dust)  map[10][10][1] = 1; // 먼지: 현재 위치
 }
 
 
 void tearDown(void) {}
 
 
-// 장애물 탐지 테스트
-// F = Front, L = Left, R = Right, Dust = 먼지 유무
+// 1. 장애물 탐지 테스트 : F = Front, L = Left, R = Right, Dust = 먼지 유무
 
 // Test0000 : F=0, L=0, R=0, Dust=0
 void test_Determine_Obstacle_0(void) {
@@ -152,14 +151,14 @@ void test_Determine_Obstacle_7(void) {
 }
 
 
-// 장애물 탐지 후 동작 테스트
+// 2. 장애물 탐지 후 동작 테스트
 
 void test_Determine_Obstacle_Location_0(void) { // state : init
-    // Case 0: ���� ��ֹ�, ����, ���� ����, ���� ���� �� TURN_RIGHT
+    // Case 0: 앞에 장애물, 왼쪽 없음, 오른쪽 없음, 먼지 없음 -> TURN_RIGHT
     TestCase tc = { 1, 0, 0, 0 };
     setup_test_case(tc);
 
-    struct robot rb = generate_robot(0); // init state(0)�� ������ �����θ� ���� state�̹Ƿ� forward state�� ����.
+    struct robot rb = generate_robot(0); // 초기 상태(init state, 0)를 나타냄
     rb.control(&rb, 1);
 
     TEST_ASSERT_TRUE(rb.motor.tr);
@@ -167,12 +166,11 @@ void test_Determine_Obstacle_Location_0(void) { // state : init
 }
 
 void test_Determine_Obstacle_Location_1(void) { // state : forward
-    // Case 1: ����, ����, ���� ��ֹ� ����, ���� ���� �� TURN_LEFT
+    // Case 1: 앞, 왼쪽, 오른쪽에 장애물 없음, 먼지 없음 -> TURN_LEFT
     TestCase tc = { 0, 0, 0, 0 };
     setup_test_case(tc);
 
-    struct robot rb = generate_robot(1); // MOVE_FORWARD ����
-
+    struct robot rb = generate_robot(1); // MOVE_FORWARD 상태
 
     rb.control(&rb, 1);
 
@@ -181,15 +179,12 @@ void test_Determine_Obstacle_Location_1(void) { // state : forward
 }
 
 void test_Determine_Obstacle_Location_2(void) { // state : forward
-    // Case 2: ���� ��ֹ�, ����, ���� ����, ���� ���� �� TURN_LEFT
+    // Case 2: 앞에 장애물, 왼쪽, 오른쪽에 장애물 없음, 먼지 없음 -> TURN_LEFT
     TestCase tc = { 1, 0, 0, 0 };
     setup_test_case(tc);
 
     struct robot rb = generate_robot(1);
     
-    // state forward�� �켱 ������ ���� �� ���� state�� �����մϴ�.
-    // ���� robot_location�� ��ĭ �ڷ� �Ű� �����մϴ�.
-
     rb.control(&rb, 1);
 
     TEST_ASSERT_TRUE(rb.motor.tl);
@@ -197,12 +192,11 @@ void test_Determine_Obstacle_Location_2(void) { // state : forward
 }
 
 void test_Determine_Obstacle_Location_3(void) { // state : forward
-    // Case 3: ���� ��ֹ�, ����, ���� ����, ���� ���� �� MOVE_FORWARD ����
+    // Case 3: 앞에 장애물 없음, 왼쪽 장애물, 오른쪽 없음, 먼지 없음 -> MOVE_FORWARD
     TestCase tc = { 0, 1, 0, 0 };
     setup_test_case(tc);
 
     struct robot rb = generate_robot(1);
-     
 
     rb.control(&rb, 1);
 
@@ -211,12 +205,11 @@ void test_Determine_Obstacle_Location_3(void) { // state : forward
 }
 
 void test_Determine_Obstacle_Location_4(void) { // state : forward
-    // Case 4: ���� ��ֹ�, ����, ���� ����, ���� ���� �� TURN_LEFT
+    // Case 4: 앞에 장애물 없음, 왼쪽 없음, 오른쪽 장애물, 먼지 없음 -> TURN_LEFT
     TestCase tc = {0, 0, 1, 0};
     setup_test_case(tc);
 
     struct robot rb = generate_robot(1);
-    
 
     rb.control(&rb, 1);
 
@@ -225,12 +218,11 @@ void test_Determine_Obstacle_Location_4(void) { // state : forward
 }
 
 void test_Determine_Obstacle_Location_5(void) { // state : forward
-    // Case 5: ���� ��ֹ�, ���� ��ֹ�, ���� ���� �� TURN_RIGHT
+    // Case 5: 앞에 장애물, 왼쪽 장애물, 오른쪽 없음, 먼지 없음 -> TURN_RIGHT
     TestCase tc = {1, 1, 0, 0};
     setup_test_case(tc);
 
     struct robot rb = generate_robot(1);
-    
 
     rb.control(&rb, 1);
 
@@ -239,12 +231,11 @@ void test_Determine_Obstacle_Location_5(void) { // state : forward
 }
 
 void test_Determine_Obstacle_Location_6(void) { // state : forward
-    // Case 6: ���� ��ֹ�, ���� ��ֹ�, ���� ���� �� TURN_LEFT
+    // Case 6: 앞에 장애물, 왼쪽 없음, 오른쪽 장애물, 먼지 없음 -> TURN_LEFT
     TestCase tc = {1, 0, 1, 0};
     setup_test_case(tc);
 
     struct robot rb = generate_robot(1);
-    
 
     rb.control(&rb, 1);
 
@@ -253,12 +244,11 @@ void test_Determine_Obstacle_Location_6(void) { // state : forward
 }
 
 void test_Determine_Obstacle_Location_7(void) { // state : forward
-    // Case 7: ����, ���� ��ֹ� ����, ���� ���� �� MOVE_FORWARD ����
+    // Case 7: 앞에 장애물 없음, 왼쪽 장애물, 오른쪽 장애물 -> MOVE_FORWARD
     TestCase tc = {0, 1, 1, 0};
     setup_test_case(tc);
 
     struct robot rb = generate_robot(1);
-    
 
     rb.control(&rb, 1);
 
@@ -267,12 +257,11 @@ void test_Determine_Obstacle_Location_7(void) { // state : forward
 }
 
 void test_Determine_Obstacle_Location_8(void) { // state : forward
-    // Case 8: ����, ����, ���� ��ֹ� ����, ���� ���� �� MOVE_BACKWARD
+    // Case 8: 앞, 왼쪽, 오른쪽 모두 장애물 -> MOVE_BACKWARD
     TestCase tc = {1, 1, 1, 0};
     setup_test_case(tc);
 
     struct robot rb = generate_robot(1);
-    
 
     rb.control(&rb, 1);
 
@@ -281,7 +270,7 @@ void test_Determine_Obstacle_Location_8(void) { // state : forward
 }
 
 void test_Determine_Obstacle_Location_9(void) { // state : turn_left
-    // Case 17: ����, ����, ����, ���� ������
+    // Case 9: 모든 방향에 장애물 없음 -> FORWARD
     TestCase tc = { 0, 0, 0, 0 };
     setup_test_case(tc);
 
@@ -294,7 +283,7 @@ void test_Determine_Obstacle_Location_9(void) { // state : turn_left
 }
 
 void test_Determine_Obstacle_Location_10(void) { // state : turn_right
-    // Case 18: ����, ����, ���� ������, ���� ��ֹ� �� turn_right
+    // Case 10: 오른쪽에 장애물 있음 -> TURN_RIGHT
     TestCase tc = { 0, 0, 1, 0 };
     setup_test_case(tc);
 
@@ -307,7 +296,7 @@ void test_Determine_Obstacle_Location_10(void) { // state : turn_right
 }
 
 void test_Determine_Obstacle_Location_11(void) { // state : turn_right
-    // Case 19: ����, ����, ���� ������, ���� ���� �� FORWARD
+    // Case 11: 모든 방향에 장애물 없음 -> FORWARD
     TestCase tc = { 0, 0, 0, 0 };
     setup_test_case(tc);
 
@@ -320,7 +309,7 @@ void test_Determine_Obstacle_Location_11(void) { // state : turn_right
 }
 
 void test_Determine_Obstacle_Location_12(void) { // state : backward
-    // Case 20: ����, ���� ������, ����, ���� ��ֹ� �� BACKWARD
+    // Case 12: 왼쪽, 오른쪽 장애물 -> BACKWARD
     TestCase tc = { 0, 1, 1, 0 };
     setup_test_case(tc);
 
@@ -333,7 +322,7 @@ void test_Determine_Obstacle_Location_12(void) { // state : backward
 }
 
 void test_Determine_Obstacle_Location_13(void) { // state : backward
-    // Case 21: ����, ���� ������, ���� ��ֹ�, ���� ���� �� TURN_RIGHT
+    // Case 13: 뒤쪽과 왼쪽 장애물 없음 -> TURN_RIGHT
     TestCase tc = { 0, 1, 0, 0 };
     setup_test_case(tc);
 
@@ -346,7 +335,7 @@ void test_Determine_Obstacle_Location_13(void) { // state : backward
 }
 
 void test_Determine_Obstacle_Location_14(void) { // state : backward
-    // Case 22: ����, ���� ������, ����, ���� ���� �� TURN_RIGHT
+    // Case 14: 뒤쪽과 오른쪽 장애물 없음 -> TURN_RIGHT
     TestCase tc = { 0, 0, 1, 0 };
     setup_test_case(tc);
 
@@ -359,7 +348,7 @@ void test_Determine_Obstacle_Location_14(void) { // state : backward
 }
 
 void test_Determine_Obstacle_Location_15(void) { // state : backward
-    // Case 23: ����, ���� ������, ����, ���� ���� �� TURN_RIGHT
+    // Case 15: 모든 방향에 장애물 없음 -> TURN_RIGHT
     TestCase tc = { 0, 0, 0, 0 };
     setup_test_case(tc);
 
@@ -372,7 +361,7 @@ void test_Determine_Obstacle_Location_15(void) { // state : backward
 }
 
 
-// ���� �������̽� �׽�Ʈ
+// 모터 인터페이스 테스트
 void test_Moter_Interface_1(void) {
     TestCase tc = { 0, 0, 0, 0 };
     setup_test_case(tc);
@@ -381,7 +370,7 @@ void test_Moter_Interface_1(void) {
     int* expected_location = calc_location(0);
 
     rb.motor.mf = true;
-    rb.motor.move(true, false, false, false); // ���� ���� ����
+    rb.motor.move(true, false, false, false); // 앞으로 이동 명령
 
     TEST_ASSERT_EQUAL_INT(expected_location[0], robot_location[0]);
     TEST_ASSERT_EQUAL_INT(expected_location[1], robot_location[1]);
@@ -393,7 +382,7 @@ void test_Moter_Interface_2(void) {
 
     struct robot rb = generate_robot(0);
     rb.motor.mf = false;
-    rb.motor.move(false, false, false, false); // ���� ���� ����
+    rb.motor.move(false, false, false, false); // 정지 명령
 
     TEST_ASSERT_EQUAL_INT(10, robot_location[0]);
     TEST_ASSERT_EQUAL_INT(10, robot_location[1]);
@@ -404,10 +393,10 @@ void test_Moter_Interface_3(void) {
     setup_test_case(tc);
 
     struct robot rb = generate_robot(0);
-    int expected_rotation = (robot_rotation - 1 + 360) % 360; // 90�� ȸ������ �Ϸ��� ���� �ʿ�!!!!!!!!!!!!!!!!!!!!
+    int expected_rotation = (robot_rotation - 1 + 360) % 360; // 왼쪽 회전 예상 각도
 
     rb.motor.tl = true;
-    rb.motor.move(false, false, true, false); // ��ȸ�� ���� ����
+    rb.motor.move(false, false, true, false); // 왼쪽 회전 명령
 
     TEST_ASSERT_EQUAL_INT(expected_rotation, robot_rotation);
 }
@@ -417,25 +406,26 @@ void test_Moter_Interface_4(void) {
     setup_test_case(tc);
 
     struct robot rb = generate_robot(0);
-    int expected_rotation = (robot_rotation + 1) % 360;
+    int expected_rotation = (robot_rotation + 1) % 360; // 오른쪽 회전 예상 각도
 
     rb.motor.tr = true;
-    rb.motor.move(false, false, false, true); // ��ȸ�� ���� ����
+    rb.motor.move(false, false, false, true); // 오른쪽 회전 명령
 
     TEST_ASSERT_EQUAL_INT(expected_rotation, robot_rotation);
 }
 
-// �κ� ���� �׽�Ʈ
+
+// 이동 동작 테스트
 void test_Move_Forward(void) {
     TestCase tc = { 0, 0, 0, 0 };
     setup_test_case(tc);
 
-    struct robot rb = generate_robot(1); // state: forward���� ����
+    struct robot rb = generate_robot(1); // 상태: forward
     int* expected_location = calc_location(0);
     int expected_x = expected_location[0];
     int expected_y = expected_location[1];
 
-    rb.control(&rb, 1); // state ��ȭ 1������ ����
+    rb.control(&rb, 1); // 상태 전환: forward
 
     TEST_ASSERT_EQUAL_INT(expected_x, robot_location[0]);
     TEST_ASSERT_EQUAL_INT(expected_y, robot_location[1]);
@@ -446,11 +436,11 @@ void test_Move_Backward(void) {
     setup_test_case(tc);
 
     struct robot rb = generate_robot(4);
-    int* expected_location = calc_location(3); // state: backward���� ����
+    int* expected_location = calc_location(3); // 상태: backward
     int expected_x = expected_location[0];
     int expected_y = expected_location[1];
 
-    rb.control(&rb, 1); // state ��ȭ 1������ ����
+    rb.control(&rb, 1); // 상태 전환: backward
 
     TEST_ASSERT_EQUAL_INT(expected_x, robot_location[0]);
     TEST_ASSERT_EQUAL_INT(expected_y, robot_location[1]);
@@ -461,9 +451,9 @@ void test_Turn_Left(void) {
     setup_test_case(tc);
 
     struct robot rb = generate_robot(2);
-    int expected_rotation = (robot_rotation - 90 + 360) % 360;
+    int expected_rotation = (robot_rotation - 90 + 360) % 360; // 왼쪽 회전 예상 각도
 
-    rb.control(&rb, 1); // state ��ȭ 1������ ����
+    rb.control(&rb, 1); // 상태 전환: turn_left
 
     TEST_ASSERT_EQUAL_INT(expected_rotation, robot_rotation);
 }
@@ -473,9 +463,9 @@ void test_Turn_Right(void) {
     setup_test_case(tc);
 
     struct robot rb = generate_robot(3);
-    int expected_rotation = (robot_rotation + 90 + 360) % 360;
+    int expected_rotation = (robot_rotation + 90 + 360) % 360; // 오른쪽 회전 예상 각도
 
-    rb.control(&rb, 1); // state ��ȭ 1������ ����
+    rb.control(&rb, 1); // 상태 전환: turn_right
 
     TEST_ASSERT_EQUAL_INT(expected_rotation, robot_rotation);
 }
@@ -498,11 +488,11 @@ void test_Exsistence_No_Dust(void) {
     struct robot rb = generate_robot(0); // 초기 상태 설정
     bool dust_detected = determine_dust_existance(rb);
 
-    TEST_ASSERT_FALSE(dust_detected); // 먼지가 감지되는지 확인
+    TEST_ASSERT_FALSE(dust_detected); // 먼지가 감지되지 않는지 확인
 }
 
 
-// Ŭ���� �������̽� �׽�Ʈ
+// 클리너 인터페이스 테스트
 void test_Cleaner_Interface_1(void) {
     TestCase tc = { 0, 0, 0, 0 };
     setup_test_case(tc);
@@ -513,32 +503,35 @@ void test_Cleaner_Interface_1(void) {
     TEST_ASSERT_FALSE(rb.cleaner.turbo);
 }
 
+// 일반 클리너 테스트 (먼지 제거 x)
 void test_Cleaner_Control_1(void) {
     TestCase tc = { 0, 0, 0, 0 };
     setup_test_case(tc);
 
     struct robot rb = generate_robot(0);
-    rb.cleaner.clean(true, false); // ���� ����
-    TEST_ASSERT_FALSE(map[robot_location[0]][robot_location[1]][1]); // ���� ���� Ȯ��
+    rb.cleaner.clean(true, false); // 일반 클리닝 모드
+    TEST_ASSERT_FALSE(map[robot_location[0]][robot_location[1]][1]);
 }
 
+// 클리너 터보 모드 테스트 (먼지 제거 o)
 void test_Cleaner_Control_2(void) {
     TestCase tc = { 0, 0, 0, 1 };
     setup_test_case(tc);
 
     struct robot rb = generate_robot(0);
-    map[robot_location[0]][robot_location[1]][1] = 1; // ���� ����
-    rb.cleaner.clean(true, true); // Ŭ���� �ͺ� �۵�
-    TEST_ASSERT_FALSE(map[robot_location[0]][robot_location[1]][1]); // ���� ���� Ȯ��
+    map[robot_location[0]][robot_location[1]][1] = 1; // 먼지 존재 설정
+    rb.cleaner.clean(true, true); // 터보 클리닝 모드
+    TEST_ASSERT_FALSE(map[robot_location[0]][robot_location[1]][1]); // 먼지가 제거되었는지 확인
 }
 
+// 클리너 종료 테스트
 void test_Cleaner_Control_3(void) {
     TestCase tc = { 0, 0, 0, 0 };
     setup_test_case(tc);
 
     struct robot rb = generate_robot(0);
-    map[robot_location[0]][robot_location[1]][1] = 0; // ���� ����
-    rb.cleaner.clean(false, false); // Ŭ���� ����
+    map[robot_location[0]][robot_location[1]][1] = 0; // 먼지 없음 설정
+    rb.cleaner.clean(false, false);  // 클리너 종료
     TEST_ASSERT_FALSE(rb.cleaner.on);
 }
 
@@ -548,7 +541,7 @@ int main(void) {
 
     map_print_flag = false;
 
-    // ��ֹ� ���� �׽�Ʈ
+    // 장애물 탐지 테스트
     RUN_TEST(test_Determine_Obstacle_0);
     RUN_TEST(test_Determine_Obstacle_1);
     RUN_TEST(test_Determine_Obstacle_2);
@@ -559,7 +552,7 @@ int main(void) {
     RUN_TEST(test_Determine_Obstacle_7);
 
 
-    // ��ֹ� ���� �� ���� �׽�Ʈ
+    // 장애물 탐지 후 동작 테스트
     RUN_TEST(test_Determine_Obstacle_Location_0);
     RUN_TEST(test_Determine_Obstacle_Location_1);
     RUN_TEST(test_Determine_Obstacle_Location_2);
@@ -577,13 +570,13 @@ int main(void) {
     RUN_TEST(test_Determine_Obstacle_Location_14);
     RUN_TEST(test_Determine_Obstacle_Location_15);
 
-     // ���� �������̽� �׽�Ʈ
+    // 모터 인터페이스 테스트
     RUN_TEST(test_Moter_Interface_1);
     RUN_TEST(test_Moter_Interface_2);
     RUN_TEST(test_Moter_Interface_3);
     RUN_TEST(test_Moter_Interface_4);
 
-    // �κ� ���� �׽�Ʈ
+    // 이동 동작 테스트
     RUN_TEST(test_Move_Forward);
     RUN_TEST(test_Move_Backward);
     RUN_TEST(test_Turn_Left);
@@ -593,7 +586,7 @@ int main(void) {
     RUN_TEST(test_Exsistence_Dust);
     RUN_TEST(test_Exsistence_No_Dust);
 
-    // Ŭ���� �������̽� �׽�Ʈ
+    // 클리너 인터페이스 테스트
     RUN_TEST(test_Cleaner_Interface_1);
     RUN_TEST(test_Cleaner_Control_1);
     RUN_TEST(test_Cleaner_Control_2);
